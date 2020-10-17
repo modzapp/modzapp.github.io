7919c85ac749c64fb333920f64d4b6db9ad526c6
Vishrut Shah <vishrutshah15@gmail.com>
2020-10-16 22:21:18 -0400

    Initial homepage setup

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyledScreen, StyledMainText, StyledSecondaryText } from './styled';
+
+function App() {
+  return (
+    <StyledScreen>
+      <StyledMainText>
+        WE'RE COMING SOON!
+      </StyledMainText>
+    </StyledScreen>
+  );
+}
+
+export default App;
---
+++ src/constant/colors.js
@@ -0,0 +1,18 @@
+export const colors = {
+    // white and its darken shades
+    white: '#ffffff',
+    whiteDarken1: '#fafafa',
+    whiteDarken2: '#f1f1f1',
+    whiteDarken3: '#bdbdbd',
+    // grey and darken
+    lightGrey: '#f7f8f9',
+    greyDark: '#888888',
+    greyDark1: '#555555',
+    // black and darkend
+    textColor: '#212121',
+    // accent colors
+    primary: '#e55458', // bright red
+    primaryColorExtraLight: '#ffe6e6', //pale red
+    secondaryColor: '#009989', // teal
+    secondaryColor2: '#1976d2', // blue 700
+}
