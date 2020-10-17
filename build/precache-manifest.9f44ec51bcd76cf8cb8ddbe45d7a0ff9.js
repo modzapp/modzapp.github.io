f3a4a1f31a0373c10473937b61b6f6dea671ac09
Vishrut Shah <vishrutshah15@gmail.com>
2020-10-16 23:23:15 -0400

    react app build deploy

---
+++ build/precache-manifest.9f44ec51bcd76cf8cb8ddbe45d7a0ff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513ebad76eb1f03301bec8af6f7801b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d2fb3ae6e7593a8937a",
+    "url": "/static/js/2.1b6e56c2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1b6e56c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af60ccab0f97abd81d3",
+    "url": "/static/js/main.2f3bdd40.chunk.js"
+  },
+  {
+    "revision": "bd7f1b59f77fe7be50dd",
+    "url": "/static/js/runtime-main.abe894a0.js"
+  }
+]);
